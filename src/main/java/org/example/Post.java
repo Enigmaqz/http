e7fdc5e38fe49a9f38cb87fa18e51c8b909ecf70
Boffka92 <boffka92@ya.ru>
2023-08-07 00:10:25 +0300

    initial commit

---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+public class Post {
+    private final String id;
+    private final String text;
+    private final String type;
+    private final String user;
+    private final int upVotes;
+    public Post(
+            @JsonProperty("id") String id,
+            @JsonProperty("text") String text,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user,
+            @JsonProperty("upvotes") int upVotes
+    ) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upVotes = upVotes;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getText() {
+        return text;
+    }
+    public String getType() {
+        return type;
+    }
+    public String getUser() {
+        return user;
+    }
+    public int getUpVotes() {
+        return upVotes;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Post" +
+                "\n id=" + id+
+                "\n text=" + text +
+                "\n type=" + type +
+                "\n user=" + user +
+                "\n votes=" + upVotes;
+    }
+}
